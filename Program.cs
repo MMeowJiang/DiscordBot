4bce3df0839cec6c024128ffc6c54ed5a3813228
喵喵醬 <110385546+MMeowJiang@users.noreply.github.com>
2025-08-24 10:49:27 +0800

    Initialize

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+string? token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+
+if (token is null) {
+    Console.WriteLine("Cannot found the token");
+    return;
+}
+
+DiscordSocketClient client = new(new DiscordSocketConfig {
+    GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages
+});
+
+client.Ready += () => {
+    _ = Task.Run(async () => await Worker());
+    return Task.CompletedTask;
+};
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
+
+await Task.Delay(-1);
+
+async Task Worker() {
+    SocketGuild guild = client.GetGuild(1392313122022227998);
+    SocketTextChannel channel = guild.GetTextChannel(1392313122504577258);
+
+    while (true) {
+        await channel.SendMessageAsync("test");
+        await Task.Delay(10000);
+    }
+}
